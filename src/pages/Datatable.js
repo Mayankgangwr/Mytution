f77a2ecf4a0baca519aa4b928c4b531823a4ca31
Mayank Gangwar <73337152+Mayankgangwr@users.noreply.github.com>
2023-07-04 20:53:01 +0000

    Update Datatable.js

--- src/pages/Datatable.js
+++ src/pages/Datatable.js
@@ -1,58 +1,116 @@
-import React, { useState } from 'react';
-import { DataTable } from 'primereact/datatable';
-import { Column } from 'primereact/column';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
+import React, { useState } from "react";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
 
 // Additional theme CSS if needed
-import 'primereact/resources/themes/saga-blue/theme.css';
+import "primereact/resources/themes/saga-blue/theme.css";
 
-import { Paginator } from 'primereact/paginator';
-import { InputText } from 'primereact/inputtext';
+import { Paginator } from "primereact/paginator";
+import { InputText } from "primereact/inputtext";
 
 function MyTable() {
-    const [data, setData] = useState([{ name: 'John', age: 25, city: 'New York' },
-    { name: 'Jane', age: 30, city: 'London' },
-    { name: 'Tom', age: 35, city: 'Paris' },
-    { name: 'Mike', age: 40, city: 'Berlin' },
-    { name: 'Emily', age: 28, city: 'Tokyo' },
-    { name: 'Chris', age: 32, city: 'Sydney' },
-    { name: 'Sarah', age: 27, city: 'Toronto' }]); // Replace [...] with your actual table data
+  const [data, setData] = useState([
+    {
+      name: "Jane",
+      standrad: 10,
+      monthfees: 1500,
+      admitat: "12/04/2023",
+      subject: "Maths, Science, English",
+    },
+    {
+      name: "Jane",
+      standrad: 10,
+      monthfees: 1500,
+      admitat: "12/04/2023",
+      subject: "Maths, Science, English",
+    },
+    {
+      name: "Jane",
+      standrad: 10,
+      monthfees: 1500,
+      admitat: "12/04/2023",
+      subject: "Maths, Science, English",
+    },
+    {
+      name: "Jane",
+      standrad: 10,
+      monthfees: 1500,
+      admitat: "12/04/2023",
+      subject: "Maths, Science, English",
+    },
+    {
+      name: "Jane",
+      standrad: 10,
+      monthfees: 1500,
+      admitat: "12/04/2023",
+      subject: "Maths, Science, English",
+    },
+    {
+      name: "Jane",
+      standrad: 10,
+      monthfees: 1500,
+      admitat: "12/04/2023",
+      subject: "Maths, Science, English",
+    },
+    {
+      name: "Jane",
+      standrad: 10,
+      monthfees: 1500,
+      admitat: "12/04/2023",
+      subject: "Maths, Science, English",
+    },
+    {
+      name: "Jane",
+      standrad: 10,
+      monthfees: 1500,
+      admitat: "12/04/2023",
+      subject: "Maths, Science, English",
+    },
+    {
+      name: "Jane",
+      standrad: 10,
+      monthfees: 1500,
+      admitat: "12/04/2023",
+      subject: "Maths, Science, English",
+    },
+  ]); // Replace [...] with your actual table data
 
-    const [first, setFirst] = useState(0);
-    const [rows, setRows] = useState(5);
-    const [sortField, setSortField] = useState('');
-    const [sortOrder, setSortOrder] = useState(null);
+  const [first, setFirst] = useState(0);
+  const [rows, setRows] = useState(5);
+  const [sortField, setSortField] = useState("");
+  const [sortOrder, setSortOrder] = useState(null);
 
-    const onPageChange = (event) => {
-        setFirst(event.first);
-        setRows(event.rows);
-    };
+  const onPageChange = (event) => {
+    setFirst(event.first);
+    setRows(event.rows);
+  };
 
-    const onSort = (event) => {
-        setSortField(event.sortField);
-        setSortOrder(event.sortOrder);
-    };
+  const onSort = (event) => {
+    setSortField(event.sortField);
+    setSortOrder(event.sortOrder);
+  };
 
-    return (
-        <div>
-            <DataTable
-                value={data}
-                paginator
-                rows={rows}
-                first={first}
-                totalRecords={data.length}
-                onPage={onPageChange}
-                sortField={sortField}
-                sortOrder={sortOrder}
-                onSort={onSort}
-            >
-                <Column field="name" header="Name" sortable filter filterPlaceholder="Search by name" />
-                <Column field="age" header="Age" sortable />
-                <Column field="city" header="City" sortable />
-            </DataTable>
-        </div>
-    );
+  return (
+    <div>
+      <DataTable
+        value={data}
+        paginator
+        rows={rows}
+        first={first}
+        totalRecords={data.length}
+        onPage={onPageChange}
+        sortField={sortField}
+        sortOrder={sortOrder}
+        onSort={onSort}
+      >
+        <Column field="name" header="Name" sortable />
+        <Column field="admitat" header="Admit At" sortable />
+        <Column field="subjects" header="subject" sortable />
+      </DataTable>
+    </div>
+  );
 }
 
 export default MyTable;
